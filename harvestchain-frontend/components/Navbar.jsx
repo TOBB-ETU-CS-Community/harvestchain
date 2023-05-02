e15838c2bb6140bef4684f0c1a65cf4e55e28a96
Recep Cankaya <recepcnky@outlook.com>
2023-05-02 18:57:19 +0300

    add navbar

--- harvestchain-frontend/components/Navbar.jsx
+++ harvestchain-frontend/components/Navbar.jsx
@@ -2,19 +2,26 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="h-12 bg-bistre flex justify-around items-center">
+    <div className="h-16 flex justify-around items-center bg-bistre">
       <h1 className="text-3xl">
         <Link href="/">HarvestChain</Link>
       </h1>
       <ul className="flex items-center gap-x-4">
         <li>
-          <Link href="">Investors</Link>
+          <Link href="" className="hover:text-yellowgreen">
+            Investors
+          </Link>
         </li>
         <li>
-          <Link href="">Growers</Link>
+          <Link href="" className="hover:text-yellowgreen">
+            Growers
+          </Link>
         </li>
       </ul>
-      <button className="">Connect Wallet</button>
+      {/*Come back later and fix border hover*/}
+      <button className="p-1 border-solid border border-aquamarine rounded-md hover:bg-asparagus hover:border-hidden">
+        Connect Wallet
+      </button>
     </div>
   );
 }
