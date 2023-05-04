28a6936a9695861415709b2a88ff694cb165700f
Recep Cankaya <recepcnky@outlook.com>
2023-05-04 19:23:13 +0300

    hide registration conditionally

--- harvestchain-frontend/components/Navbar.jsx
+++ harvestchain-frontend/components/Navbar.jsx
@@ -1,26 +1,34 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { ContextAPI } from "../context/ContextProvider";
 
 export default function Navbar() {
+  const { address, connectWallet, walletConnected } = useContext(ContextAPI);
+
+  const hideRegister = walletConnected ? "hidden" : "";
+
   return (
     <div className="h-16 flex justify-around items-center bg-bistre">
       <h1 className="text-3xl">
         <Link href="/">HarvestChain</Link>
       </h1>
       <ul className="flex items-center gap-x-4">
-        <li>
-          <Link href="" className="hover:text-yellowgreen">
-            Investors
-          </Link>
+        <li className={`${hideRegister}  hover:text-yellowgreen`}>
+          <Link href="">Investors</Link>
         </li>
         <li>
-          <Link href="" className="hover:text-yellowgreen">
-            Growers
-          </Link>
+          <Link href="">Profile</Link>
+        </li>
+        <li className={`${hideRegister} hover:text-yellowgreen`}>
+          <Link href="">Growers</Link>
         </li>
       </ul>
-      {/*Come back later and fix border hover*/}
-      <button className="p-1 border-solid border border-aquamarine rounded-md hover:bg-asparagus hover:border-hidden">
-        Connect Wallet
+      <button
+        className="p-1.5 border-solid border border-aquamarine rounded-md hover:bg-asparagus hover:border-asparagus"
+        onClick={connectWallet}>
+        {walletConnected
+          ? `Wallet Connected: ${address.slice(0, 5)}...${address.slice(-4)}`
+          : "Connect Wallet"}
       </button>
     </div>
   );
