a09e228c2086156d902ed155d68205b812295ce6
Recep Cankaya <recepcnky@outlook.com>
2023-05-05 19:06:30 +0300

    add footer

---
+++ harvestchain-frontend/components/Footer.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Footer() {
+  return (
+    <footer className="flex-[0_0_50px] mt-24">
+      <div className="w-full h-0.5 bg-gradient-to-r from-yellowgreen to-screamingreen"></div>
+      <h3 className="flex justify-center py-6 bg-bistre">
+        <span>
+          <Image src="/logo.png" alt="Logo" width={25} height={25} />
+        </span>{" "}
+        &#10084; Made By Blockchain Club of TOBB Ecoonomics and Technology
+      </h3>
+    </footer>
+  );
+}
--- harvestchain-frontend/src/pages/_app.js
+++ harvestchain-frontend/src/pages/_app.js
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { ContextProvider } from "../../context/ContextProvider";
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <ContextProvider>
       <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </ContextProvider>
   );
 }
