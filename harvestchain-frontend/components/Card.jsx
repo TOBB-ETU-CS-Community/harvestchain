cd1c0a51dbfaabe54ecd5a60e502ead04b41ce89
Recep Cankaya <recepcnky@outlook.com>
2023-05-09 15:43:37 +0300

    add hover effect to images

--- harvestchain-frontend/components/Card.jsx
+++ harvestchain-frontend/components/Card.jsx
@@ -1,16 +1,25 @@
 import { cardDetails } from "./mock";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Card() {
   const productList = cardDetails.map((card) => (
-    <div key={card.id} className="">
-      <Image src="/ejder-meyvesi.jpg" alt="Fruit" width={200} height={200} />
+    <Link href="" key={card.id}>
+      <div className="w-48 h-48 overflow-hidden">
+        <Image
+          src="/ejder-meyvesi.jpg"
+          alt="Fruit"
+          width={200}
+          height={200}
+          className="transform transition duration-300 hover:scale-110"
+        />
+      </div>
       <h1>{card.grower}</h1>
       <p>{card.details}</p>
       <p>{card.capital}</p>
       <p>{card.expectedReturn}</p>
       <p>{card.endTime}</p>
-    </div>
+    </Link>
   ));
 
   return (
--- harvestchain-frontend/components/Navbar.jsx
+++ harvestchain-frontend/components/Navbar.jsx
@@ -13,28 +13,31 @@ export default function Navbar() {
     : "border-aquamarine";
 
   return (
-    <div className="h-16 flex justify-around items-center bg-bistre">
-      <h1 className="text-3xl">
-        <Link href="/">HarvestChain</Link>
-      </h1>
-      <ul className="flex items-center gap-x-4">
-        <li className={`${hideRegister} hover:text-yellowgreen`}>
-          <Link href="/investor">Investors</Link>
-        </li>
-        <li>
-          <Link href="/profile">Profile</Link>
-        </li>
-        <li className={`${hideRegister} hover:text-yellowgreen`}>
-          <Link href="/grower">Growers</Link>
-        </li>
-      </ul>
-      <button
-        className={`p-1.5 ${walletBg} border-solid border ${walletBorder} rounded-md hover:bg-asparagus hover:border-asparagus`}
-        onClick={connectWallet}>
-        {walletConnected
-          ? `Wallet Connected: ${address.slice(0, 5)}...${address.slice(-4)}`
-          : "Connect Wallet"}
-      </button>
-    </div>
+    <>
+      <nav className="h-16 flex justify-around items-center bg-bistre">
+        <h1 className="text-3xl">
+          <Link href="/">HarvestChain</Link>
+        </h1>
+        <ul className="flex items-center gap-x-4">
+          <li className={`${hideRegister} hover:text-yellowgreen`}>
+            <Link href="/investor">Investors</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li className={`${hideRegister} hover:text-yellowgreen`}>
+            <Link href="/grower">Growers</Link>
+          </li>
+        </ul>
+        <button
+          className={`p-1.5 ${walletBg} border-solid border ${walletBorder} rounded-md hover:bg-asparagus hover:border-asparagus`}
+          onClick={connectWallet}>
+          {walletConnected
+            ? `Wallet Connected: ${address.slice(0, 5)}...${address.slice(-4)}`
+            : "Connect Wallet"}
+        </button>
+      </nav>
+      <div className="w-full h-0.5 bg-gradient-to-r from-yellowgreen to-screamingreen"></div>
+    </>
   );
 }
