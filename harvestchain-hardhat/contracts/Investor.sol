3caf5a39a858a7375e1dda002ab3e00587af0181
Recep Cankaya <recepcnky@outlook.com>
2023-05-12 23:33:09 +0300

    fix the manufacturer contract

--- harvestchain-hardhat/contracts/Investor.sol
+++ harvestchain-hardhat/contracts/Investor.sol
@@ -2,50 +2,67 @@
 pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Investor is ReentrancyGuard{
-    
+contract Investor is ReentrancyGuard {
+    address public immutable growerContract;
+
     struct Investors {
         address client;
         uint amount;
         uint balance;
         string userName;
     }
-    mapping (address => Investors) public investors;
-    
 
-    function getUserName (string memory _userName) public { //get user name from investor
-        require(bytes(investors[msg.sender].userName).length == 0, "User already registered");
-        Investors storage investor = investors[msg.sender];
-        investor.userName = _userName ;
-        investor.client = msg.sender;
-        investor.balance = _getInvestorBalance();
-        
-        
+    mapping(address => Investors) public investors;
+
+    constructor() {
+        growerContract = address(0);
     }
 
-    function getPayment() public payable{  //get payment from investor
+    function getUserName(string memory _userName) public {
+        //get user name from investor
+        require(
+            bytes(investors[msg.sender].userName).length == 0,
+            "User already registered"
+        );
+        Investors storage investor = investors[msg.sender];
+        investor.userName = _userName;
+        investor.client = msg.sender;
+        investor.balance = _getInvestorBalance();
+    }
+
+    function takePayment() public payable {
+        //get payment from investor
         Investors storage investor = investors[msg.sender];
         investor.amount = msg.value;
     }
 
     function sendPayment() public payable nonReentrant {
         Investors storage investor = investors[msg.sender];
-        require(investor.client == msg.sender,"You are not allowed to take any token");
+        require(
+            investor.client == msg.sender,
+            "You are not allowed to take any token"
+        );
         uint _amount = investor.amount;
-        investor.amount  = 0 ;
-        (bool ok,) = msg.sender.call{value: _amount}(" ");
+        investor.amount = 0;
+        (bool ok, ) = msg.sender.call{value: _amount}(" ");
         require(ok);
     }
 
-    
+    function sendToGrower(uint256 amount) external payable nonReentrant {
+        require(
+            msg.sender == investors[msg.sender].client,
+            "You are not allowed"
+        );
+        amount = msg.value;
+        (bool ok, ) = msg.sender.call{value: amount}("");
+        require(ok);
+    }
 
-    function getContractBalance() public view returns(uint) {
+    function getContractBalance() public view returns (uint) {
         return address(this).balance;
     }
 
-    function _getInvestorBalance() internal view returns(uint) {
+    function _getInvestorBalance() internal view returns (uint) {
         return address(msg.sender).balance;
     }
-
-    
 }
--- harvestchain-hardhat/contracts/ManufacturerRegistry.sol
+++ harvestchain-hardhat/contracts/ManufacturerRegistry.sol
@@ -3,10 +3,10 @@ pragma solidity ^0.8.17;
 
 contract ManufacturerRegistry {
     struct Manufacturer {
-    string name;
-    string location;
-    uint256 registrationDate;
-    uint256 balance;
+        string name;
+        string location;
+        uint256 registrationDate;
+        uint256 balance;
     }
 
     struct Advertisement {
@@ -19,20 +19,52 @@ contract ManufacturerRegistry {
     }
 
     mapping(address => Manufacturer) public manufacturers;
-    mapping(address => Advertisement[]) public manufacturerAdvertisements;
+    mapping(uint256 => Advertisement) public manufacturerAdvertisements;
 
-    uint256 private advertisementId = 1;
+    uint256 public advertisementId;
 
-    event ManufacturerRegistered(address indexed manufacturerAddress, string name, string location, uint256 registrationDate);
-    event ManufacturerUpdated(address indexed manufacturerAddress, string name, string location, uint256 balance, uint256 updatedDate);
+    event ManufacturerRegistered(
+        address indexed manufacturerAddress,
+        string name,
+        string location,
+        uint256 registrationDate
+    );
+    event ManufacturerUpdated(
+        address indexed manufacturerAddress,
+        string name,
+        string location,
+        uint256 balance,
+        uint256 updatedDate
+    );
     event PaymentReceived(address indexed manufacturerAddress, uint256 amount);
-    event AdvertisementCreated(address indexed manufacturerAddress, uint256 advertisementId, string productName, uint256 area, uint256 duration, uint256 estimatedRevenue, uint256 createdDate);
-    event AdvertisementRetrieved(address indexed manufacturerAddress, uint256 advertisementId, string productName, uint256 area, uint256 duration, uint256 estimatedRevenue, uint256 creationDate);
-    event AdvertisementDeleted(address indexed manufacturerAddress, uint256 advertisementId);
+    event AdvertisementCreated(
+        address indexed manufacturerAddress,
+        uint256 advertisementId,
+        string productName,
+        uint256 area,
+        uint256 duration,
+        uint256 estimatedRevenue,
+        uint256 createdDate
+    );
+    event AdvertisementRetrieved(
+        address indexed manufacturerAddress,
+        uint256 advertisementId,
+        string productName,
+        uint256 area,
+        uint256 duration,
+        uint256 estimatedRevenue,
+        uint256 creationDate
+    );
 
-    function registerManufacturer(string calldata name, string calldata location) external {
+    function registerManufacturer(
+        string calldata name,
+        string calldata location
+    ) external {
         require(bytes(name).length > 0, "Manufacturer name is required");
-        require(bytes(location).length > 0, "Manufacturer location is required");
+        require(
+            bytes(location).length > 0,
+            "Manufacturer location is required"
+        );
 
         manufacturers[msg.sender] = Manufacturer({
             name: name,
@@ -41,57 +73,93 @@ contract ManufacturerRegistry {
             balance: _getBalance()
         });
 
-        emit ManufacturerRegistered(msg.sender, name, location, block.timestamp);
+        emit ManufacturerRegistered(
+            msg.sender,
+            name,
+            location,
+            block.timestamp
+        );
     }
 
-    function updateManufacturer(string calldata name, string calldata location) external {
+    function updateManufacturer(
+        string calldata name,
+        string calldata location
+    ) external {
         require(bytes(name).length > 0, "Manufacturer name is required");
-        require(bytes(location).length > 0, "Manufacturer location is required");
+        require(
+            bytes(location).length > 0,
+            "Manufacturer location is required"
+        );
 
         Manufacturer storage manufacturer = manufacturers[msg.sender];
         manufacturer.name = name;
         manufacturer.location = location;
 
-        emit ManufacturerUpdated(msg.sender, name, location, manufacturer.balance, block.timestamp);
+        emit ManufacturerUpdated(
+            msg.sender,
+            name,
+            location,
+            manufacturer.balance,
+            block.timestamp
+        );
     }
 
-    function createAdvertisement(string calldata productName, uint256 area, uint256 duration, uint256 estimatedRevenue) external {
+    function createAdvertisement(
+        string calldata productName,
+        uint256 area,
+        uint256 estimatedRevenue
+    ) external returns (uint256) {
         require(bytes(productName).length > 0, "Product name is required");
 
         Manufacturer storage manufacturer = manufacturers[msg.sender];
-        require(manufacturer.balance > 0, "Balance should be greater than zero");
+        require(
+            manufacturer.balance > 0,
+            "Balance should be greater than zero"
+        );
 
-        Advertisement memory advertisement = Advertisement({
-            id: advertisementId,
-            productName: productName,
-            area: area,
-            duration: duration,
-            estimatedRevenue: estimatedRevenue,
-            creationDate: block.timestamp
-        });
+        Advertisement storage advertisement = manufacturerAdvertisements[
+            advertisementId
+        ];
+        advertisement.productName = productName;
+        advertisement.area = area;
+        advertisement.estimatedRevenue = estimatedRevenue;
+        advertisement.duration = block.timestamp + 1 minutes;
 
-        manufacturerAdvertisements[msg.sender].push(advertisement);
-
-        emit AdvertisementCreated(msg.sender, advertisementId, productName, area, duration, estimatedRevenue, block.timestamp);
+        emit AdvertisementCreated(
+            msg.sender,
+            advertisementId,
+            productName,
+            area,
+            advertisement.duration,
+            estimatedRevenue,
+            block.timestamp
+        );
 
         advertisementId++;
+        return advertisementId - 1;
     }
 
-    function retrieveAdvertisementById(uint256 _advertisementId) external view returns (string memory productName, uint256 area, uint256 duration, uint256 estimatedRevenue, uint256 creationDate) {
-        Advertisement[] storage advertisements = manufacturerAdvertisements[msg.sender];
-        require(_advertisementId > 0 && _advertisementId <= advertisements.length, "Invalid advertisement ID");
-
-        Advertisement storage advertisement = advertisements[advertisementId - 1];
-        return (advertisement.productName, advertisement.area, advertisement.duration, advertisement.estimatedRevenue, advertisement.creationDate);
-    }
-
-    function deleteAdvertisement(uint256 _advertisementId) external {
-        Advertisement[] storage advertisements = manufacturerAdvertisements[msg.sender];
-        require(_advertisementId > 0 && _advertisementId <= advertisements.length, "Invalid advertisement ID");
-
-        delete advertisements[_advertisementId - 1];
-
-        emit AdvertisementDeleted(msg.sender, _advertisementId);
+    function retrieveAdvertisementById(
+        uint256 _advertisementId
+    )
+        external
+        view
+        returns (string memory, uint256, uint256, uint256, uint256)
+    {
+        require(
+            _advertisementId > 0 && _advertisementId <= advertisementId,
+            "Invalid advertisement ID"
+        );
+        Advertisement storage advertisement = manufacturerAdvertisements[
+            _advertisementId
+        ];
+        return (
+            advertisement.productName,
+            advertisement.area,
+            advertisement.duration,
+            advertisement.estimatedRevenue,
+            advertisement.creationDate
+        );
     }
 
     function receivePayment() external payable {
@@ -105,4 +173,6 @@ contract ManufacturerRegistry {
     function _getBalance() internal view returns (uint256) {
         return msg.sender.balance;
     }
+
+    receive() external payable {}
 }
