23d20e0769a6eabcaede087b2859ae05c83c7651
Recep Cankaya <recepcnky@outlook.com>
2023-05-13 10:23:28 +0300

    inital deploy

--- harvestchain-hardhat/hardhat.config.js
+++ harvestchain-hardhat/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "hardhat",
+  networks: {
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
