90f93c1f31bda3a727c397cad58eb9c55f505018
Recep Cankaya <recepcnky@outlook.com>
2023-05-14 00:59:08 +0300

    display proposals on main page

--- harvestchain-frontend/components/Card.jsx
+++ harvestchain-frontend/components/Card.jsx
@@ -1,30 +1,60 @@
-import { cardDetails } from "./mock";
+import { useState, useContext, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { ContextAPI } from "../context/ContextProvider";
 
 export default function Card() {
-  const productList = cardDetails.map((card) => (
-    <Link href={card.id} key={card.id}>
-      <div className="w-48 h-48 overflow-hidden">
-        <Image
-          src="/ejder-meyvesi.jpg"
-          alt="Fruit"
-          width={200}
-          height={200}
-          className="rounded-md transform transition duration-300 hover:scale-105"
-        />
-      </div>
-      <h1>{card.grower}</h1>
-      <p>{card.area}</p>
-      <p>{card.capital}</p>
-      <p>{card.estimatedReturn}</p>
-      <p>{card.duration}</p>
-    </Link>
-  ));
+  const [proposals, setProposals] = useState([]);
+  const { growerContractInstance, getProviderOrSigner } =
+    useContext(ContextAPI);
+
+  const proposalList = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const contract = await growerContractInstance(provider);
+      let i = 0;
+      let done = false;
+      setProposals([]);
+      do {
+        try {
+          const oneProposal = await contract.manufacturerAdvertisements(i);
+          console.log(oneProposal);
+          setProposals((props) => [...props, oneProposal]);
+          i++;
+        } catch (error) {
+          done = true;
+          console.error(error);
+        }
+      } while (!done && i < proposals.length);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    proposalList();
+  }, []);
 
   return (
     <div className="grid grid-cols-4 row-auto justify-items-center gap-y-8 mt-8">
-      {productList}
+      {proposals.map((card) => (
+        <Link href={String(card.id)} key={Number(card.id)}>
+          <div className="w-48 h-48 overflow-hidden">
+            <Image
+              src="/ejder-meyvesi.jpg"
+              alt="Fruit"
+              width={200}
+              height={200}
+              className="rounded-md transform transition duration-300 hover:scale-105"
+            />
+          </div>
+          <p className="font-bold">
+            Product Name: {card.productName.toString()}
+          </p>
+          <p>Area: {card.area.toString()}</p>
+          <p>Estimated Revenue: {card.estimatedRevenue.toString()}%</p>
+        </Link>
+      ))}
     </div>
   );
 }
--- harvestchain-frontend/src/pages/[proposalId].js
+++ harvestchain-frontend/src/pages/[proposalId].js
@@ -1,55 +1,54 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { cardDetails } from "../../components/mock";
 import Line from "../../components/Line";
 
 export default function ProposalDetails() {
   const router = useRouter();
   const proposalId = router.query.proposalId;
 
-  const productDetail = cardDetails.map((card) => {
-    if (card.id === proposalId) {
-      return (
-        <div className="flex items-center" key={card.id}>
-          <Image
-            src="/ejder-meyvesi.jpg"
-            alt="Fruit"
-            width={200}
-            height={200}
-            className="rounded-md"
-          />
-          <div>
-            <div className="flex items-center">
-              <Line />
-              <h2 className="mb-2">{card.grower}</h2>
-            </div>
-            <div className="flex items-center">
-              <Line />
-              <p className="mb-2">{card.area}</p>
-            </div>
-            <div className="flex items-center">
-              <Line />
-              <p className="mb-2">{card.capital}</p>
-            </div>
-            <div className="flex items-center">
-              <Line />
-              <p className="mb-2">{card.estimatedReturn}</p>
-            </div>
-            <div className="flex items-center">
-              <Line />
-              <p>{card.duration}</p>
-            </div>
-          </div>
-        </div>
-      );
-    }
-    return null;
-  });
+  // const productDetail = cardDetails.map((card) => {
+  //   if (card.id === proposalId) {
+  //     return (
+  //       <div className="flex items-center" key={card.id}>
+  //         <Image
+  //           src="/ejder-meyvesi.jpg"
+  //           alt="Fruit"
+  //           width={200}
+  //           height={200}
+  //           className="rounded-md"
+  //         />
+  //         <div>
+  //           <div className="flex items-center">
+  //             <Line />
+  //             <h2 className="mb-2">{card.grower}</h2>
+  //           </div>
+  //           <div className="flex items-center">
+  //             <Line />
+  //             <p className="mb-2">{card.area}</p>
+  //           </div>
+  //           <div className="flex items-center">
+  //             <Line />
+  //             <p className="mb-2">{card.capital}</p>
+  //           </div>
+  //           <div className="flex items-center">
+  //             <Line />
+  //             <p className="mb-2">{card.estimatedReturn}</p>
+  //           </div>
+  //           <div className="flex items-center">
+  //             <Line />
+  //             <p>{card.duration}</p>
+  //           </div>
+  //         </div>
+  //       </div>
+  //     );
+  //   }
+  //   return null;
+  // });
 
   return (
     <div className="flex justify-center items-center">
       <div className="mt-24 overflow-hidden">
-        {productDetail}
+        {/* {productDetail} */}
         <div className="w-1/3 flex flex-col mt-12 float-right ">
           <input
             className="mb-4 pl-2 h-8 rounded-lg text-black focus:outline-0 placeholder:italic"
